a750a4dd54c96e49a3a027e0c2b94a1038c15f5e
Alex Zhu <alex.t.zhu+github@gmail.com>
2015-05-08 08:23:05 -0500

    Weather icons

--- js/new-tab.js
+++ js/new-tab.js
@@ -75,3 +75,41 @@ newTabApp.service('WeatherResource', function($resource, config){
             + apiKey + '/' + latitude + ',' + longitude);
     };
 });
+
+// Convert forecast.io icons to weather-icons
+newTabApp.filter('iconFilter', function(){
+    return function(icon){
+        switch(icon){
+            case 'clear-day':
+                return 'wi-day-sunny';
+            case 'clear-night':
+                return 'wi-night-clear';
+            case 'rain':
+                return 'wi-rain';
+            case 'snow':
+                return 'wi-snow';
+            case 'sleet':
+                return 'wi-day-sleet';
+            case 'wind':
+                return 'wi-strong-wind';
+            case 'fog':
+                return 'wi-fog';
+            case 'cloudy':
+                return 'wi-cloudy';
+            case 'partly-cloudy-day':
+                return 'wi-day-cloudy';
+            case 'partly-cloudy-night':
+                return 'wi-night-partly-cloudy';
+        }
+    };
+});
+
+// Bearing is reported in direction wind is from, but icons have it backwards.
+// Also round to closest 15 degrees
+newTabApp.filter('windBearingFilter', function(){
+    return function(bearing){
+        var newBearing = Math.round(bearing / 15) * 15;
+        newBearing = (newBearing + 180) % 360;
+        return newBearing;
+    };
+})
