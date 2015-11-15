ac22fbbdff3388bc6577e245554769a27a5128f1
Alex Zhu <alex.t.zhu@gmail.com>
2015-11-15 03:43:15 -0800

    Remove api keys

--- js/apiKeys.js
+++
@@ -1,5 +0,0 @@
-angular.module('api_keys', [])
-.constant('api_keys', {
-    'googleApiKey': 'AIzaSyA1uj5WIzQTEq1qPbbU3oiyWzBsijdJ32U',
-    'forecastioApiKey': 'd22e9c4fef4e11cf0028dc7a42699e8d'
-});
