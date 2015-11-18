05b1ecdcfff61d6cdc7101c1dcc4b22b92dc9348
Alex Zhu <alexazhu@amazon.com>
2015-11-18 14:21:46 -0800

    Remove day/night icons for today/tomorrow; documentation

--- js/directives/weatherDirective.js
+++ js/directives/weatherDirective.js
@@ -2,11 +2,11 @@ newTabApp.directive('weatherSnippet', function() {
     return {
         restrict: 'E',
         scope: {
-            data : '=',
-            summary : '=',
-            day : '=',
-            label : '@',
-            showPrecipDetail : '='
+            data : '=',             // Weather data
+            summary : '=',          // Summary sentence
+            day : '=',              // Day data - use for day/night specific icons
+            label : '@',            // Title to display
+            showPrecipDetail : '='  // Show precipitation intensity?
         },
         templateUrl: '../../templates/weather.html'
     };
