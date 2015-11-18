78fb3de097a97bfa69535d8c57cf25583bf74db7
Alex Zhu <alexazhu@amazon.com>
2015-11-18 15:47:02 -0800

    Wind bearing improvements; tooltip

--- js/filters/windBearingFilter.js
+++
@@ -1,9 +0,0 @@
-// Bearing is reported in direction wind is from, but icons have it backwards.
-// Also round to nearest 15 degrees
-newTabApp.filter('windBearingFilter', function(){
-    return function(bearing){
-        var newBearing = Math.round(bearing / 15) * 15;
-        newBearing = (newBearing + 180) % 360;
-        return newBearing;
-    };
-})
