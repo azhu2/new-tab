a32341a16e48d4743f51e6695fb02c419f09cf0e
Alex Zhu <alex.t.zhu@gmail.com>
2018-05-28 21:56:04 -0700

    Fix location updating typo

--- js/utils/locationUtils.js
+++ js/utils/locationUtils.js
@@ -8,7 +8,7 @@ newTabApp.service('LocationUtils', function() {
         }
 
         // 1 minute ~= 1 mile depending on latitude
-        var shouldUpdate = Math.max(Math.abs(location.latitude - lastLocation.Latitude), Math.abs(location.longitude - lastLocation.Longitude)) > 1/60;
+        var shouldUpdate = Math.max(Math.abs(location.latitude - lastLocation.latitude), Math.abs(location.longitude - lastLocation.longitude)) > 1/60;
 
         if (shouldUpdate) {
             lastLocation = location;
