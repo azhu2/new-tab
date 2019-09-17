f7aaed7cf3883c91475fd22697ec4af6ea21ed75
Alex Zhu <alex@alexazhu.com>
2019-09-17 10:39:32 -0700

    Add location permissions prompt

--- js/controllers/pageController.js
+++ js/controllers/pageController.js
@@ -5,16 +5,59 @@ newTabApp.controller('pageController',
 
     function init() {
         updateTime();
+        $interval(updateTime, 1000);
+
+        checkLocationPermissions().then(function() {
+            postInit();
+        });
+    };
+
+    init();
+
+    function checkLocationPermissions() {
+        var deferred = $q.defer();
+
+        if (window.chrome && chrome.extension) {
+            console.log('Running as an extension; permissions check not needed');
+            $scope.permissionsGranted = true;
+            deferred.resolve();
+        }
+
+        navigator.permissions.query({name: 'geolocation'}).then(function(result) {
+            if (result.state === 'granted') {
+                console.info('Location permissions granted');
+                $scope.permissionsGranted = true;
+                deferred.resolve();
+            }
+
+            result.onchange = function() {
+                if (result.state === 'granted') {
+                    console.info('Location permissions granted');
+                    $scope.permissionsGranted = true;
+                    deferred.resolve();
+                } else {
+                    console.warn('Location permissions not granted; functionality will be limited');
+                    $scope.permissionsGranted = false;
+                }
+            }
+        });
+
+        // Query location to force permissions prompt
+        updateLocation();
+        return deferred.promise;
+    }
+
+    /**
+     * Run after location permissions granted.
+     */
+    function postInit() {
         updateLocation();
 
-        $interval(updateTime, 1000);
         $interval(updateLocation, 10000);
         $interval(updateWeather, 300000);
 
         $rootScope.$on('updateWeather', updateWeather);
-    };
-
-    init();
+    }
 
     function updateTime() {
         $scope.time = Date.now();
